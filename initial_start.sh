2ef80075a8bcf2896248cdad08409af76f2b71f2
blofse <blofse@users.noreply.github.com>
2017-06-05 13:57:43 +0000

    Fixed script with newer networking style of docker provisioning

--- initial_start.sh
+++ initial_start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-docker run --name nexus -p 8081:8081 -v NexusData:/nexus-data -d sonatype/nexus3
+docker network create \
+  --driver bridge \
+  nexus-network
+
+docker run \
+  --name nexus-data \
+  --net nexus-network \
+  -d \
+  sonatype/nexus3 \
+  echo "data-only container for Nexus"
+
+docker run \
+  --name nexus \
+  --net nexus-network \
+  --volumes-from nexus-data \
+  -p 8081:8081 \
+  -d \
+  sonatype/nexus3
 
 echo Done!
